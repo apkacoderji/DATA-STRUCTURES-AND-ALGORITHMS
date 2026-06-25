eb21141dec65a5ff9d9de5949117069b94f78b28
apkacoderji <150407686+apkacoderji@users.noreply.github.com>
2026-06-25 18:12:11 +0530

    Time: 150 ms (5.15%), Space: 20.7 MB (62.89%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,23 +1,33 @@
 class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        freq = {}
-        left = 0
-        longest_substring = 0
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
 
-        for right in range(len(s)):
-            freq[s[right]] = freq.get(s[right],0) + 1
+        max_water = 0
 
-            k = right-left+1
+        def calculateWater(i,j):
+            length = min(height[i],height[j])
+            width = j-i
 
-            while len(freq) < k:
-                freq[s[left]] -= 1
-                if freq[s[left]] == 0:
-                    del freq[s[left]]
-                left += 1
-                k = right-left+1
+            return length*width
+        
+        i = 0
+        j = len(height) - 1
+        while i < j:
             
-            longest_substring = max(longest_substring,right-left+1)
             
+            max_water = max(max_water,calculateWater(i,j))
 
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+         
 
-        return longest_substring
+        
+        return max_water
+
+                
+        
